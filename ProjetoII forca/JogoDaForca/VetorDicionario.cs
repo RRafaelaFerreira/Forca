e4fea4026270010238050e06eeaad58d71c26248
RRafaelaFerreira <130802955+RRafaelaFerreira@users.noreply.github.com>
2025-06-11 20:53:56 -0300

    Update VetorDicionario.cs

--- ProjetoII forca/JogoDaForca/VetorDicionario.cs
+++ ProjetoII forca/JogoDaForca/VetorDicionario.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,30 @@ namespace JogoDaForca
     internal class VetorDicionario
     {
         private Dicionario[] Dados;
+        private int posicaoAtual;
+
+        public VetorDicionario(int capacidade)
+        {
+            Dados = new Dicionario[capacidade];
+            posicaoAtual = 0;
+        }
+
+        public void Adicionar(Dicionario item)
+        {
+            if (posicaoAtual < Dados.Length)
+            {
+                Dados[posicaoAtual] = item;
+                posicaoAtual++;
+            }
+        }
+
+        public void Listar()
+        {
+            for (int i = 0; i < posicaoAtual; i++)
+            {
+                Console.WriteLine(Dados[i].ToString());
+            }
+        }
 
     }
 }
